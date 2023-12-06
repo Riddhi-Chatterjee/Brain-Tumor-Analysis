78f33cbf90e348a997bfad0e0f4843feb77dc6d2
Ankrutee <ankruteearora888@gmail.com>
2023-12-07 00:45:58 +0530

    fixing networking issue

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,33 +3,30 @@ import requests
 
 app = Flask(__name__)
 
-# Home route
 @app.route("/")
 def main():
     return render_template("index.html")
 
-# Prediction route
 @app.route('/prediction', methods=['POST'])
 def predict_image_file():
     try:
         if request.method == 'POST':
-            # Get the image file from the request
-            image_file = request.files['file'].stream
+            # Assuming you have a form with a file input named 'file'
+            image_file = request.files['file']
 
-            # Send a request to the Flask backend on port 6000 for prediction
+            # Sending a request to your backend for prediction
             response = requests.post("http://127.0.0.1:6000/predict", files={'file': image_file})
 
             if response.status_code == 200:
-                # Get the prediction from the response
-                prediction = response.json()['prediction']
+                # Getting the prediction from the response
+                prediction = response.json().get('prediction', 'Error: No prediction received')
                 return render_template("result.html", predictions=str(prediction))
             else:
-                error = "Error in prediction request."
+                error = "Error in prediction request. Status code: {}".format(response.status_code)
                 return render_template("result.html", err=error)
 
     except Exception as e:
         error = "Error: File can't be processed"
-        
         return render_template("result.html", err=error)
 
 if __name__ == "__main__":
